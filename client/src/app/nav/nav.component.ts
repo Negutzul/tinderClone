e105dea2c91bdb7036eb87d82bfb9aa9f6f23594
Negutzul <neaguandrei47@yahoo.com>
2025-03-07 23:46:04 +0200

    register and login and pages

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+@Component({
+  selector: 'app-nav',
+  imports: [FormsModule, CommonModule, BsDropdownModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css',
+})
+export class NavComponent {
+  
+  accountService = inject(AccountService)
+  model: any = {};
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response)
+      },
+      error: er => console.log(er)
+    })
+    console.log(this.model);
+  }
+
+  logout(){
+    this.accountService.logout();
+  }
+
+}
