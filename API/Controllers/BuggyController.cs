3c087134995239f0348e54a443a9cfa1d9920b79
Negutzul <neaguandrei47@yahoo.com>
2025-04-10 15:08:27 +0300

    gata inca o buc

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,37 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController(DataContext context) : BaseApiController
+{
+    [Authorize]
+    [HttpGet("auth")]
+    public ActionResult<string> GetAuth(){
+        return "nanana";
+    }
+
+    [HttpGet("not-found")]
+    public ActionResult<AppUser> GetNotFound(){
+        var thing = context.Users.Find(-1);
+        if(thing == null) return NotFound();
+        return thing;
+    }
+    
+    [HttpGet("server-error")]
+    public ActionResult<AppUser> GetServerError(){
+
+            var thing = context.Users.Find(-1) ?? throw new Exception("a bad thing happpend");
+            
+            return thing;
+        
+    }
+    
+    [HttpGet("bad-request")]
+    public ActionResult<string> GetBadRequest(){
+        return BadRequest("not good req nanana");
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using API.Extensions;
+using API.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,6 +15,8 @@ builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
